ddbab630ebe29e23518b14d7e013deb28b73a34c
ice-cube-1 <alice.goward@spgs.org>
2024-08-09 12:33:45 +0100

    redone gui

--- neatish.py
+++ neatish.py
@@ -1,3 +1,4 @@
+from sys import argv
 import tkinter as tk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -5,24 +6,27 @@ import math
 
 class Line():
     def __init__(self, input_str: dict[str, str], minmax: int) -> None:
-        self.g: float = float(input_str['g']); self.minmax: int = minmax
+        self.g: float = float(input_str['g']); self.minmax: int = minmax            
         self.k: float = 0 if input_str['k'] == '' else float(input_str['k'])
         self.N: float = 0 if input_str['N'] == '' else float(input_str['N'])
         self.c: float = 0 if input_str['c'] == '' else float(input_str['c'])
         self.h: float = 0 if input_str['h'] == '' else float(input_str['h'])
-        if input_str['t'] == '':
-            if input_str['u'] == '':
-                self.u: float = self.minU(float(input_str['X']), float(input_str['Y']))
-                self.t: tuple = self.tFromU(float(input_str['X']), float(input_str['Y']))
-            else:
-                self.u: float = float(input_str['u'])
-                if input_str['X'] == '': self.t: tuple = (self.maxRange(),)
-                else: self.t: tuple = self.tFromU(float(input_str['X']), float(input_str['Y']))
-        else: self.t: tuple = (math.radians(float(input_str['t'])),); self.u: float = float(input_str['u'])
+        if options.get() == 1: 
+            self.t: tuple = (math.radians(float(input_str['t'])),)
+            self.u: float = float(input_str['u'])
+        elif options.get() == 2: 
+            self.u = self.minU(float(input_str['X']), float(input_str['Y']))
+            self.t: tuple = self.tFromU(float(input_str['X']), float(input_str['Y']))
+        elif options.get() == 3: 
+            self.u = float(input_str['u'])
+            self.t: tuple = (self.maxRange(),)
+        else: 
+            self.u = float(input_str['u'])
+            self.t: tuple = self.tFromU(float(input_str['X']), float(input_str['Y']))
 
     def plot(self) -> None:
         for t in self.t:
-            if self.N == 0 and self.k == 0: data: list[tuple[float, float]] = self.line(t)
+            if bounce_verlet.get() != 1: data: list[tuple[float, float]] = self.line(t)
             else: data: list[tuple[float, float]] = self.bounceVerlet(t)
             self.getRange(t)
             plt.plot([i[0] for i in data], [i[1] for i in data])
@@ -101,20 +105,55 @@ def update_plot():
     for i in lines: i.plot(); i.print_info()
     canvas.draw()
 
-
+def toggle_inputs(*args):
+    # First, hide all widgets
+    for key in input_str.keys():
+        entry_labels[key][0].pack_forget()
+        entry_labels[key][1].pack_forget()
+    pack(['g','h'])
+    if options.get() == 1:
+        pack(['t','u'])
+    elif options.get() == 2:
+        pack(['X','Y'])
+    elif options.get() == 3:
+        pack(['u'])
+    elif options.get() == 4:
+        pack(['X','Y','t','u'])
+    if bounce_verlet.get() == 1:
+        pack(['N','k','c'])
+def pack(topack: list[str]):
+    for i in topack:
+        entry_labels[i][0].pack()
+        entry_labels[i][1].pack()
+        
 input_str: dict[str, str] = {'g': '10', 't': '45', 'u': '2', 'h': '0', 'X': '', 'Y': '', 'N': '', 'k': '', 'c': ''}
-lines: list[Line] = [Line(input_str, 0)]; entry_labels: dict[str, list] = {}
 root = tk.Tk()
 root.title("Projectile Motion Plot")
 fig, ax = plt.subplots()
 canvas = FigureCanvasTkAgg(fig, master=root)
 canvas.get_tk_widget().pack()
+options = tk.IntVar(value=1)
+lines: list[Line] = [Line(input_str, 0)]; entry_labels: dict[str, list] = {}
+r1 = tk.Radiobutton(root, text="Path with angle / velocity", value=1, variable=options, command=lambda: toggle_inputs(options.get())).pack()
+r2 = tk.Radiobutton(root, text="Minimum velocity to pass through XY", value=2, variable=options, command=lambda: toggle_inputs(options.get())).pack()
+r3 = tk.Radiobutton(root, text="Maximum range for a given speed", value=3, variable=options, command=lambda: toggle_inputs(options.get())).pack()
+r4 = tk.Radiobutton(root, text="Angle to pass through XY at a given speed", value=4, variable=options, command=lambda: toggle_inputs(options.get())).pack()
+
 for i in input_str:
-    entry_labels[i] = [tk.Label(root, text=f'{i}:').pack(), tk.Entry(root)]
-    entry_labels[i][1].insert(0, str(input_str[i])); entry_labels[i][1].pack()
+    label = tk.Label(root, text=f'{i}:')
+    label.pack()
+    entry = tk.Entry(root)
+    entry.insert(0, str(input_str[i]))
+    entry.pack()
+    entry_labels[i] = [label, entry]
+
 minmax = tk.IntVar()
-checkbutton = tk.Checkbutton(root, text="Show local minmax", variable=minmax, onvalue=1, offvalue=0).pack()
+checkbutton = tk.Checkbutton(root, text="Show local minmax (if applicable)", variable=minmax, onvalue=1, offvalue=0).pack()
 update_button = tk.Button(root, text="Update Plot", command=update_plot).pack()
 save_button = tk.Button(root, text="Save Line", command=save_line).pack()
 reset_button = tk.Button(root, text="Reset Lines", command=reset_lines).pack()
+bounce_verlet = tk.IntVar()
+bounce_verlet_checkbox = tk.Checkbutton(root, text="Bounce Verlet", variable=bounce_verlet, onvalue=1, offvalue=0, command=toggle_inputs)
+bounce_verlet_checkbox.pack()
+
 root.mainloop()
